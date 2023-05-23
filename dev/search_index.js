4a570ebb2a81801f4fa31922b92b5967597a4b07
Documenter.jl <documenter@juliadocs.github.io>
2023-05-23 12:45:06 +0000

    build based on 8b1cca9

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Trickling_up","category":"page"},{"location":"#Trickling_up","page":"Home","title":"Trickling_up","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Trickling_up.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Trickling_up]","category":"page"}]
+}
