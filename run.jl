7f0b8e2575aa1695739b978b8170649a0c84a4ee
Martin Bernstein <martinmbernstein@gmail.com>
2023-05-28 11:49:39 +0200

    time series length test

--- run.jl
+++ run.jl
@@ -5,11 +5,11 @@ using TestSetExtensions
 
 include("src/run_estimates.jl")
 
-# Unpack the results
+# Unpack the results 
 a_pe, C_pe, a, C,
 a1, C1, a2, C2, 
 a3, C3, a_re, C_re,
-a_mp, C_mp, ts = run_estimates()
+a_mp, C_mp, ts = run_estimates(print=true)
 
 # Define a function for stack plots
 include("src/stack_plot.jl")
